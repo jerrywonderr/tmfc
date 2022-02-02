8e98725deb6de6a07988bb994f1af024f9062a26
Joseph Jeremiah <jerrycul2001@gmail.com>
2022-02-02 07:53:01 +0100

    corrected nav display on mobile devices

--- TMFC/settings.py
+++ TMFC/settings.py
@@ -151,10 +151,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-STATIC_URL = '/static/'
-MEDIA_URL = "/media/"
-STATIC_ROOT = BASE_DIR / "static"
+if DEVELOPMENT_MODE:
+    STATIC_URL = '/static/'
+    MEDIA_URL = "/media/"
+    STATIC_ROOT = BASE_DIR / "static"
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
